5c51a6281ded9ddb0ab79cacfdcaa35dc87ed5a0
N-mika <nadrasanamichael@gmail.com>
2025-09-16 16:03:12 +0300

    map app

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
-// app.js
 const express = require("express");
-const app = express();
-const db = require("./db");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const http = require("http");
+const socketInit = require("./src/sockets/sockets.js");
+const socialMediaRoutes = require("./src/routes/socialMedia.js");
+const userRouter = require("./src/routes/userRoute.js");
+const file = require("./src/routes/file.js");
+const code = require("./src/routes/codeInscription.js");
+const folder = require("./src/routes/folder.js");
+const chat = require("./src/routes/chat.js");
+const connectMongo = require("./src/db/db.js");
 const env = require("dotenv");
-const Users = require("./model");
 env.config();
-db.connectDB();
-// Port configuré dans Jelastic ou port par défaut 8080
-const port = process.env.PORT || 8080;
 
-app.get("/user", async (req, res) => {
-  try {
-    const users = await Users.find().select("-password"); // Ne pas renvoyer les passwords
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ message: "Erreur serveur", error: err });
-  }
+connectMongo();
+const port = process.env.PORT || 8080
+// pour le socket.io
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
-app.post("/user", (req, res) => {
-  res.status(200).json("User");
-});
-app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Jelastic Cloud Infomaniak!");
-});
+// le cors
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+socketInit(io);
 
-app.listen(port, () => {
-  console.log(`Server running and listening on port ${port}`);
+app.use(express.json());
+app.use("/auth", userRouter);
+app.use("/users", userRouter);
+app.use("/folder", folder);
+app.use("/file", file);
+app.use("/code", code);
+app.use("/chat", chat);
+app.use("/social", socialMediaRoutes);
+
+server.listen(port, () => {
+  console.log(`Serveur lancer`);
 });
