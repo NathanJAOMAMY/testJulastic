575d8d8f1bcd87e1ed7e3956b44234cec0e8493d
N-mika <nadrasanamichael@gmail.com>
2025-09-16 11:59:07 +0300

    test

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // app.js
 const express = require('express');
 const app = express();
-
+const db = require("./db");
+const env = require("dotenv")
+env.config()
+db.connectDB();
 // Port configuré dans Jelastic ou port par défaut 8080
 const port = process.env.PORT || 8080;
 
+app.post('/user', (req , res)=>{
+  res.send(req.body);
+})
 app.get('/', (req, res) => {
   res.send('Hello from Node.js on Jelastic Cloud Infomaniak!');
 });
